7fc8cac85af30e3057c2f2e79904fd99ab22a496
ezrafollower <ezrafollower@gmail.com>
2022-07-07 22:29:28 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
-from setuptools import setup, find_packages
+import os
+from setuptools import setup
+from setuptools.extension import Extension
 
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+pyivp = Extension(
+    name='pyivp',
+    include_dirs=[os.path.join(this_dir, 'src/lib_geometry')],
+    sources=[
+        os.path.join(this_dir, 'src/pybind11', 'XYPoint.cpp'),
+        os.path.join(this_dir, 'src/pybind11', 'XYSegList.cpp')
+    ]
+)
 
 setup(
-    name='example_publish_pypi_medium',
-    version='0.6',
-    license='MIT',
-    author="Giorgos Myrianthous",
-    author_email='email@example.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
-    keywords='example project',
-    install_requires=[
-          'scikit-learn',
-      ],
-
+    name='pyivp',
+    version='0.0.1',
+    author='ARG',
+    description='pyivp module',
+    long_description='using pybind11 to bind moosivp into python library',
+    url='https://github.com/ARG-NCTU/pyivp.git',
+    classifiers=[
+        # The list of PyPI classifiers
+    ],
+    ext_modules=[pyivp],
+    zip_safe=False,
+    include_package_data=True,
 )
